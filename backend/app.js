d1dfd629a3021acce4a5d9bcc5f11d5612bd11c1
Yanis <yan4on@yandex.ru>
2021-03-01 01:17:35 +0300

    исправил CORS

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const helmet = require('helmet');
+const cors = require('cors');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -15,6 +16,31 @@ app.use(helmet());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+app.use(cors());
+
+const allowedCors = [
+  'localhost:3000',
+  'localhost:3001',
+  'https://yan4on.students.nomoredomains.icu/',
+  'http://yan4on.students.nomoredomains.icu/',
+  'https://api.yan4on.students.nomoredomains.icu/',
+  'http://api.yan4on.students.nomoredomains.icu/',
+];
+
+app.use(cors());
+app.use((req, res, next) => {
+  const { origin } = req.headers; // Записываем в переменную origin соответствующий заголовок
+
+  // eslint-disable-next-line max-len
+  if (allowedCors.includes(origin)) { // Проверяем, что значение origin есть среди разрешённых доменов
+    res.header('Access-Control-Allow-Origin', origin);
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  }
+
+  next();
+});
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
   useNewUrlParser: true,
