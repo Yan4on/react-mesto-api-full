4086d22eb20cfd8d872796b87292880834e8d8ae
Yanis <yan4on@yandex.ru>
2021-02-28 21:26:11 +0300

    bug fix crash test

--- backend/app.js
+++ backend/app.js
@@ -23,6 +23,11 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
 app.post('/signin', login);
 app.post('/signup', createUser);
 app.use('/users', routerUsers);
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,5 +1,4 @@
 import React, { useState, memo } from 'react';
-import { useHistory } from 'react-router-dom';
 import FormSign from './FormSign';
 
 const Register = memo(({ changeCurrUrl, authRegister, inputPass }) => {
@@ -7,7 +6,6 @@ const Register = memo(({ changeCurrUrl, authRegister, inputPass }) => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const history = useHistory();
     // Обработчик ввода данных в поля
     function handleChange(e) {
 
