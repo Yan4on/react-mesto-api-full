8cce5efb5158496cba54547e6355b548f1009e9e
zed-chi <zed-chi@mail.ru>
2021-03-06 23:54:06 +0300

    changed response json

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -2,22 +2,37 @@ const Card = require('../models/card');
 const NotFoundError = require('../errors/not-found-err');
 const BadRequestError = require('../errors/bad-request-err');
 const ForbiddenError = require('../errors/forbidden-err');
+const { JWT_SECRET = 'secret' } = process.env;
+const jwt = require('jsonwebtoken');
+
+function getTokenFromReq(req){
+  var header = req.headers["authorization"];
+  return token = header.split(" ")[1];
+}
+function getIdFromToken(token){  
+    const decoded = jwt.verify(token, JWT_SECRET);
+    return decoded._id;
+}
+function getIdFromReqHeader(req){
+  const token = getTokenFromReq(req);
+  const id = getIdFromToken(token);
+  return id;
+}
 
 module.exports.getCards = (req, res, next) => {
   Card.find({})
     .then((cards) => {
-      if (!cards.length) {
-        res.send({ data: [] });
-      }
-      res.send({ data: cards });
+      res.setHeader('Content-Type', 'application/json');      
+      return res.send(JSON.stringify(cards));
     })
     .catch(next);
 };
 
-module.exports.createCard = (req, res, next) => {
+module.exports.createCard = (req, res, next) => {  
+  const id = getIdFromReqHeader(req);
   const { name, link } = req.body;
-  Card.create({ name, link, owner: req.user._id })
-    .then((card) => res.status(201).send({ data: card }))
+  Card.create({ name, link, owner: id })
+    .then((card) => res.status(201).send(card))
     .catch((err) => {
       let error;
       if (err.name === 'ValidationError') {
@@ -29,37 +44,42 @@ module.exports.createCard = (req, res, next) => {
 };
 
 module.exports.deleteCard = (req, res, next) => {
+  const id = getIdFromReqHeader(req);
   Card.findById(req.params.id)
     .orFail(new NotFoundError('Нет карточки с таким id'))
     .then((card) => {
-      if (!card.owner.equals(req.user._id)) {
+      if (!card.owner.equals(id)) {
         throw new ForbiddenError('Удалить можно только свою карточку');
       }
 
-      Card.deleteOne(card).then(() => res.send({ data: card }));
+      Card.deleteOne(card).then(() => res.send(card));
     })
     .catch(next);
 };
 
 module.exports.likeCard = (req, res, next) => {
+  const id = getIdFromReqHeader(req);
   Card.findByIdAndUpdate(
     req.params.id,
-    { $addToSet: { likes: req.user._id } }, { new: true },
+    { $addToSet: { likes: id } }, { new: true },
   )
     .orFail(new NotFoundError('Нет карточки с таким id'))
     .then((card) => {
-      res.send({ data: card });
+      res.send(card);
     })
     .catch(next);
 };
 
-module.exports.dislikeCard = (req, res, next) => Card.findByIdAndUpdate(
+module.exports.dislikeCard = (req, res, next) => {
+  const id = getIdFromReqHeader(req);
+
+  return Card.findByIdAndUpdate(
   req.params.id,
-  { $pull: { likes: req.user._id } },
+  { $pull: { likes: id } },
   { new: true },
 )
   .orFail(new NotFoundError('Нет карточки с таким id'))
   .then((card) => {
-    res.send({ data: card });
+    res.send(card);
   })
-  .catch(next);
+  .catch(next);}
